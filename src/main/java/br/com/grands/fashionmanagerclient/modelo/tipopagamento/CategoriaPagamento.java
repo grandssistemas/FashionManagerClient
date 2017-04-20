192afb34d3668ed6a9a90e9f47976640ab9c233b
Luiz Augusto <luizaugusto@sgsistemas.com.br>
2017-04-20 10:13:27 -0300

    Alterando os atributos de private para public e tipando atributos

--- src/main/java/br/com/grands/fashionmanagerclient/modelo/tipopagamento/CategoriaPagamento.java
+++ src/main/java/br/com/grands/fashionmanagerclient/modelo/tipopagamento/CategoriaPagamento.java
@@ -7,11 +7,11 @@ import java.util.List;
 public class CategoriaPagamento {
 
     @ApiModelProperty(value = "ID da categoria de pagamento do software que esta usando a integração",position = 1, required = true)
-    private Long id;
+    public Long id;
     @ApiModelProperty(value = "Nome da categoria de pagamento", position = 2, required = true)
-    private String nome;
+    public String nome;
     @ApiModelProperty(value = "Lista dos tipos de pagamento da categoria", position = 3, required = true)
-    private List<TipoPagamento> tipoPagamento;
+    public List<TipoPagamento> tipoPagamento;
 
     public CategoriaPagamento() {
     }
@@ -19,30 +19,6 @@ public class CategoriaPagamento {
     public CategoriaPagamento(Long id, String nome, List<TipoPagamento> tipoPagamento) {
         this.id = id;
         this.nome = nome;
-        this.tipoPagamento = tipoPagamento;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<TipoPagamento> getTipoPagamento() {
-        return tipoPagamento;
-    }
-
-    public void setTipoPagamento(List<TipoPagamento> tipoPagamento) {
         this.tipoPagamento = tipoPagamento;
     }
 }
---
+++ src/main/java/br/com/grands/fashionmanagerclient/modelo/tipopagamento/enums/FormaPagamento.java
@@ -0,0 +1,8 @@
+package br.com.grands.fashionmanagerclient.modelo.tipopagamento.enums;
+
+/**
+ * Created by luizaugusto on 20/04/17.
+ */
+public enum FormaPagamento {
+    ESPECIE,BANCARIO,OUTROS
+}
