74834c7fee4368c0fc9475986851a468c7301d7b
Luiz Augusto <luizaugusto@sgsistemas.com.br>
2017-04-20 10:01:33 -0300

    Alterando os atributos de private para public

--- src/main/java/br/com/grands/fashionmanagerclient/modelo/combustivel/Abastecimento.java
+++ src/main/java/br/com/grands/fashionmanagerclient/modelo/combustivel/Abastecimento.java
@@ -9,26 +9,26 @@ import java.util.Date;
 public class Abastecimento {
 
     @ApiModelProperty(value = "Id do abastecimento no sistema que está realizando a integração", required = true, position = 1)
-    private Long id;
+    public Long id;
     @ApiModelProperty(value = "Número do bico em que o abastecimento foi realizado", required = true, position = 2)
-    private Long numeroBico;
+    public Long numeroBico;
 
     @ApiModelProperty(value = "Data em que foi realizado o abastecimento, no formato DD/MM/YYYY", required = true, position = 3)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
-    private Date dataAbastecimento;
+    public Date dataAbastecimento;
 
     @ApiModelProperty(value = "Horário em que foi realizado o abastecimento, no formato HH:MM:SS", required = true, position = 4)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss",  locale = "pt-BR", timezone = "America/Sao_Paulo")
-    private Date horaAbastecimento;
+    public Date horaAbastecimento;
 
     @ApiModelProperty(value = "Total do encerrante antes do abastecimento", required = true, position = 5)
-    private BigDecimal encerrante;
+    public BigDecimal encerrante;
 
     @ApiModelProperty(value = "Quantidade que foi abastecida.", required = true, position = 6)
-    private BigDecimal quantidade;
+    public BigDecimal quantidade;
 
     @ApiModelProperty(value = "Id da pessoa responsável pelo abastecimento. São aceitas somente pessoas que tenham o papel de funcionário.", required = true, position = 7)
-    private Long frentista;
+    public Long frentista;
 
     public Abastecimento() {
     }
@@ -40,62 +40,6 @@ public class Abastecimento {
         this.horaAbastecimento = horaAbastecimento;
         this.encerrante = encerrante;
         this.quantidade = quantidade;
-        this.frentista = frentista;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getNumeroBico() {
-        return numeroBico;
-    }
-
-    public void setNumeroBico(Long numeroBico) {
-        this.numeroBico = numeroBico;
-    }
-
-    public Date getDataAbastecimento() {
-        return dataAbastecimento;
-    }
-
-    public void setDataAbastecimento(Date dataAbastecimento) {
-        this.dataAbastecimento = dataAbastecimento;
-    }
-
-    public Date getHoraAbastecimento() {
-        return horaAbastecimento;
-    }
-
-    public void setHoraAbastecimento(Date horaAbastecimento) {
-        this.horaAbastecimento = horaAbastecimento;
-    }
-
-    public BigDecimal getEncerrante() {
-        return encerrante;
-    }
-
-    public void setEncerrante(BigDecimal encerrante) {
-        this.encerrante = encerrante;
-    }
-
-    public BigDecimal getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(BigDecimal quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public Long getFrentista() {
-        return frentista;
-    }
-
-    public void setFrentista(Long frentista) {
         this.frentista = frentista;
     }
 }
