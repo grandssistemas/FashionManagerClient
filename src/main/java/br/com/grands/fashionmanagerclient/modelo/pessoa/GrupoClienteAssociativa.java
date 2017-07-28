3e27677ae4bd18c4f1f474012e26aeb9ad7d55c2
portolan <matheusportolan@gmail.com>
2017-07-28 15:55:05 -0300

    api de integração de grupo de cliente

--- src/main/java/br/com/grands/fashionmanagerclient/modelo/pessoa/GrupoClienteAssociativa.java
+++ src/main/java/br/com/grands/fashionmanagerclient/modelo/pessoa/GrupoClienteAssociativa.java
@@ -1,7 +1,19 @@
 package br.com.grands.fashionmanagerclient.modelo.pessoa;
 
-/**
- * Created by cesar on 28/07/2017.
- */
+import io.swagger.annotations.ApiModelProperty;
+
 public class GrupoClienteAssociativa {
+
+    @ApiModelProperty(value = "Status do cliente. True representa ativo e False inativo. O valor padao desse campo é TRUE", position = 1, required = true)
+    public Boolean ativo = Boolean.TRUE;
+    @ApiModelProperty(value = "Nome fantasia do cliente", position = 2, required = true)
+    public String nomefantasia;
+    @ApiModelProperty(value = "Razão social do cliente", position = 3, required = true)
+    public String razaosocial;
+    @ApiModelProperty(value = "Raz do cliente", position = 4)
+    public String raiz;
+    @ApiModelProperty(value = "CNPJ do cliente", position = 5, required = true)
+    public String cnpj;
+    @ApiModelProperty(value = "Pessoa vinculada com esse cliente", position = 6)
+    public Pessoa pessoa;
 }
